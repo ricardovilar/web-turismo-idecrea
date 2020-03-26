ce84f3db621b7e94dd3224020e9ab69655f83ec3
Ricardo Vilar <vilar.ricardo@gmail.com>
2020-03-26 12:44:26 +0100

    First commit

---
+++ precache-manifest.180d28f5d643d8bf4c7b269e5d53f092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19a9e99292717a239cc",
+    "url": "https://ricardovilar.github.io/web-turismo-idecrea/css/app.f12061f3.css"
+  },
+  {
+    "revision": "e39e7e8b3c9d3f6b25f821693c7660ab",
+    "url": "https://ricardovilar.github.io/web-turismo-idecrea/index.html"
+  },
+  {
+    "revision": "d19a9e99292717a239cc",
+    "url": "https://ricardovilar.github.io/web-turismo-idecrea/js/app.3b74ce3d.js"
+  },
+  {
+    "revision": "dcefc19107ec80f8ea33",
+    "url": "https://ricardovilar.github.io/web-turismo-idecrea/js/chunk-vendors.d0a63c46.js"
+  },
+  {
+    "revision": "ca9313a580fde5a0fcfea5b3f3107554",
+    "url": "https://ricardovilar.github.io/web-turismo-idecrea/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://ricardovilar.github.io/web-turismo-idecrea/robots.txt"
+  }
+]);
